bd160a90f6dcb995170435206258df85274e7e37
abhir20 <37292638+abhir20@users.noreply.github.com>
2023-08-26 17:29:25 -0400

    Add project files.

---
+++ AuthenticationTokenRetriever/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Reddit;
+
+
+namespace AuthenticationTokenRetriever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reddit = new RedditClient(appId: "YourRedditAppID", appSecret: "YourRedditAppSecret", refreshToken: "YourBotUserRefreshToken");
+
+        }
+    }
+}
---
+++ RedditModels/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedditModels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
